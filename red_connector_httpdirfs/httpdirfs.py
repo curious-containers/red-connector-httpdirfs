ad55fb4b36b9d959a01c88f43f853bfe3606813d
Bluemi <bruno.schilling@protonmail.ch>
2019-02-14 13:47:35 +0100

    inital commit

---
+++ red_connector_httpdirfs/httpdirfs.py
@@ -0,0 +1,12 @@
+class HttpDirFs:
+    @staticmethod
+    def receive_directory(access, internal, listing):
+        pass
+
+    @staticmethod
+    def receive_directory_validate(access):
+        pass
+
+    @staticmethod
+    def receive_directory_cleanup(internal):
+        pass
